bce750011f51b5513d6e1ce35468f8408c7ed8b6
Mateus Silva de Almeida <75602797+MateusAlmeida0@users.noreply.github.com>
2026-04-20 14:18:22 -0300

    Update Carta.py

---
+++ Carta.py
@@ -0,0 +1,29 @@
+class Carta:
+    """Representa uma carta do baralho com valor e naipe."""
+
+    def __init__(self, valor, naipe):
+        """Inicializa a carta com valor (ex: 'A', '10') e naipe."""
+        self.valor = valor
+        self.naipe = naipe
+
+    def valor_blackjack(self):
+        """Retorna o valor numérico da carta no Blackjack."""
+        if self.valor == 'A':
+            return 11
+        elif self.valor == 'J' or self.valor == 'Q' or self.valor == 'K':
+            return 10
+        else:
+            return int(self.valor)
+
+    def valor_hilo(self):
+        """Retorna o valor da carta para contagem Hi-Lo."""
+        if self.valor in ['2','3','4','5','6']:
+            return 1
+        elif self.valor in ['7','8','9']:
+            return 0
+        else:
+            return -1
+
+    def __str__(self):
+        """Retorna a representação da carta como texto."""
+        return self.valor + " de " + self.naipe
